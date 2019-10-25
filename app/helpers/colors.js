4fedf06e390e70d65b912d6fa08330d7dee8c2c6
GangadharSwami <gangadhar.mswami@gmail.com>
2019-10-25 12:24:06 +0530

    added colors file

---
+++ app/helpers/colors.js
@@ -0,0 +1,12 @@
+export default {
+  Green: '#226557',
+  InactiveGreen: '#3BAD87',
+  Balck: '',
+  White: '#ffffff',
+  Orange: '#f69b31',
+  Red: '#f60c0d',
+  InactiveRed: '#f65a22',
+  Blue: '#615af6',
+  InactiveBlue: '#46f6d7',
+  LightBlue: '#67baf6'
+}
