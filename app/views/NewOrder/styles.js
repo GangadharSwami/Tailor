61b700157f7bc6fa15c158ca150f124ec97bab22
GangadharSwami <gangadhar.mswami@gmail.com>
2019-10-25 13:12:29 +0530

    added style for NewOrder

---
+++ app/views/NewOrder/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import  Colors from '../../helpers/colors';
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: Colors.primaryBackground,
+  },
+});
+
+export default styles;
