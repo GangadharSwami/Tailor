8969231fcf521eba2d5d7bf9e57864b92f6785c1
GangadharSwami <gangadhar.mswami@gmail.com>
2019-10-25 14:40:57 +0530

    added file to store temp data

---
+++ app/views/NewOrder/data.js
@@ -0,0 +1,19 @@
+export const Clients = [
+  {
+    id: 1,
+    name: "Gangadhar",
+    mobile: '1234567890'
+  },{
+    id: 2,
+    name: "Sagar",
+    mobile: '098765421'
+  },{
+    id: 3,
+    name: "client 3",
+    mobile: '57890431'
+  },{
+    id: 4,
+    name: "client 5",
+    mobile: '03743828'
+  }
+]
