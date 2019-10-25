7483f11d649d401adf0a780a9ec47ac4cd92f33b
GangadharSwami <gangadhar.mswami@gmail.com>
2019-10-25 14:38:21 +0530

    added input for select client

--- app/views/NewOrder/index.js
+++ app/views/NewOrder/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, ScrollView, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Colors from '../../helpers/colors';
+import styles from './styles';
+import { Input, Button } from 'react-native-elements';
 
 export default class NewOrder extends Component{
   static navigationOptions = {
@@ -15,10 +17,35 @@ export default class NewOrder extends Component{
     barStyle: { backgroundColor: Colors.LightBlue },
   }
 
+  _renderIcon = (name) => {
+    return(
+      <Icon
+        name={name}
+        size={20}
+        color='#2089dc'
+      />
+    )
+  }
+
   render() {
     return(
-      <View style={{ flex: 1, justifyContent: 'center' ,alignItems: 'center', }}> 
-        <Text>This is NewOrder screen</Text>
+      <View style={ styles.mainContainer }> 
+        <ScrollView>
+          <View style={ styles.clientNameContainer }>
+            <Input
+              placeholder='Select Client / Enter name'
+              leftIcon={ this._renderIcon("account") }
+              containerStyle={{width: '80%', padding: 0}}
+            />
+            <Button
+              icon={ this._renderIcon('account-plus') }
+              type="outline"
+              title="Add"
+              iconContainerStyle={{ paddingRight: 20}}
+              containerStyle = {{width: '20%', justifyContent: 'center'}}
+            />
+          </View>
+        </ScrollView>
       </View>
     );
   }
