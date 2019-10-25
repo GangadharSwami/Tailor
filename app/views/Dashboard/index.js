49fa56c6554a652dafdef847a6c1186728e41c1b
GangadharSwami <gangadhar.mswami@gmail.com>
2019-10-25 12:49:08 +0530

    moved navigationOptions to Dashboard/index.js

--- app/views/Dashboard/index.js
+++ app/views/Dashboard/index.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Colors from '../../helpers/colors';
 
 export default class Dashboard extends Component{
+  static navigationOptions = {
+    tabBarLabel:'Dashboard',
+    tabBarIcon: ({ tintColor }) => (
+      <View>
+        <Icon style={[{color: tintColor}]} size={25} name={'ios-home'}/>
+      </View>),
+    activeColor: Colors.White,
+    inactiveColor: Colors.InactiveGreen,
+    barStyle: { backgroundColor: Colors.Green },
+  }
+
   render() {
     return(
       <View style={{ flex: 1, justifyContent: 'center' ,alignItems: 'center', }}> 
--- app/views/Home/index.js
+++ app/views/Home/index.js
@@ -1,32 +1,17 @@
-import React from 'react';
-import { View } from 'react-native';
 import { createAppContainer} from 'react-navigation';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
-import Icon from 'react-native-vector-icons/Ionicons';
 import Dashboard from '../Dashboard/index';
 import PendingOrders from '../PendingOrders/index';
 import NewOrder from '../NewOrder/index';
-import Colors from '../../helpers/colors';
 
 const TabNavigator = createMaterialBottomTabNavigator(  
   {
-    Dashboard: { screen: Dashboard,
-      navigationOptions:{
-        tabBarLabel:'Dashboard',
-        tabBarIcon: ({ tintColor }) => (
-          <View>
-            <Icon style={[{color: tintColor}]} size={25} name={'ios-home'}/>
-          </View>),
-      }  
-    },  
+    Dashboard: { screen: Dashboard },  
     PendingOrders: { screen: PendingOrders },
     NewOrder: { screen: NewOrder }
   },
   {
     initialRouteName: "Dashboard",
-    activeColor: Colors.White,
-    inactiveColor: Colors.InactiveGreen,
-    barStyle: { backgroundColor: Colors.Green },
   },
 );
 
