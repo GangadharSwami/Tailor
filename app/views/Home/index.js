386b6a18e89895ac0d14e1be429953a2ff3c0799
GangadharSwami <gangadhar.mswami@gmail.com>
2019-10-25 12:04:31 +0530

    added bottom navigator in spp

--- app/views/Home/index.js
+++ app/views/Home/index.js
@@ -1,13 +1,54 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import { createAppContainer} from 'react-navigation';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Dashboard from '../Dashboard/index';
+import PendingOrders from '../PendingOrders/index';
+import NewOrder from '../NewOrder/index';
 
-export default class Home extends Component{
-  render() {
-    return(
-      <View> 
-        <Text>This is home screen</Text>
-      </View>
-    );
-  }
-   
-}
+const TabNavigator = createMaterialBottomTabNavigator(  
+  {  
+    Dashboard: { screen: Dashboard,  
+      navigationOptions:{ 
+        tabBarLabel:'Dashboard',  
+        tabBarIcon: ({ tintColor }) => (
+          <View>  
+            <Icon style={[{color: tintColor}]} size={25} name={'ios-home'}/>  
+          </View>),  
+      }  
+    },  
+    PendingOrders: { screen: PendingOrders,
+      navigationOptions:{  
+        tabBarLabel:'Pending Order',  
+        tabBarIcon: ({ tintColor }) => (  
+          <View>  
+              <Icon style={[{color: tintColor}]} size={25} name={'ios-person'}/>  
+          </View>),  
+        activeColor: '#f60c0d',  
+        inactiveColor: '#f65a22',  
+        barStyle: { backgroundColor: '#f69b31' },  
+      }  
+    },  
+    NewOrder: { screen: NewOrder,  
+      navigationOptions:{  
+        tabBarLabel:'NewOrder',  
+        tabBarIcon: ({ tintColor }) => (  
+            <View>  
+              <Icon style={[{color: tintColor}]} size={25} name={'ios-images'}/>  
+            </View>),  
+        activeColor: '#615af6',  
+        inactiveColor: '#46f6d7',  
+        barStyle: { backgroundColor: '#67baf6' },  
+      }  
+    } 
+  },  
+  {  
+    initialRouteName: "Dashboard",  
+    activeColor: '#f0edf6',  
+    inactiveColor: '#226557',  
+    barStyle: { backgroundColor: '#3BAD87' },  
+  },  
+);  
+
+export default createAppContainer(TabNavigator);  
