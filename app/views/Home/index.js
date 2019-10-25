eacf405167f2910a96595e55514c58b44df92fe0
GangadharSwami <gangadhar.mswami@gmail.com>
2019-10-25 12:26:38 +0530

    fixed colors

--- app/views/Home/index.js
+++ app/views/Home/index.js
@@ -6,49 +6,50 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import Dashboard from '../Dashboard/index';
 import PendingOrders from '../PendingOrders/index';
 import NewOrder from '../NewOrder/index';
+import Colors from '../../helpers/colors';
 
 const TabNavigator = createMaterialBottomTabNavigator(  
-  {  
-    Dashboard: { screen: Dashboard,  
-      navigationOptions:{ 
-        tabBarLabel:'Dashboard',  
+  {
+    Dashboard: { screen: Dashboard,
+      navigationOptions:{
+        tabBarLabel:'Dashboard',
         tabBarIcon: ({ tintColor }) => (
-          <View>  
-            <Icon style={[{color: tintColor}]} size={25} name={'ios-home'}/>  
-          </View>),  
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'ios-home'}/>
+          </View>),
       }  
     },  
     PendingOrders: { screen: PendingOrders,
-      navigationOptions:{  
-        tabBarLabel:'Pending Order',  
-        tabBarIcon: ({ tintColor }) => (  
-          <View>  
-              <Icon style={[{color: tintColor}]} size={25} name={'ios-person'}/>  
-          </View>),  
-        activeColor: '#f60c0d',  
-        inactiveColor: '#f65a22',  
-        barStyle: { backgroundColor: '#f69b31' },  
-      }  
-    },  
-    NewOrder: { screen: NewOrder,  
-      navigationOptions:{  
-        tabBarLabel:'NewOrder',  
-        tabBarIcon: ({ tintColor }) => (  
-            <View>  
-              <Icon style={[{color: tintColor}]} size={25} name={'ios-images'}/>  
-            </View>),  
-        activeColor: '#615af6',  
-        inactiveColor: '#46f6d7',  
-        barStyle: { backgroundColor: '#67baf6' },  
-      }  
-    } 
-  },  
-  {  
-    initialRouteName: "Dashboard",  
-    activeColor: '#f0edf6',  
-    inactiveColor: '#226557',  
-    barStyle: { backgroundColor: '#3BAD87' },  
-  },  
-);  
+      navigationOptions:{
+        tabBarLabel:'Pending Order',
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'ios-person'}/>
+          </View>),
+        activeColor: Colors.Red,
+        inactiveColor: Colors.InactiveRed,
+        barStyle: { backgroundColor: Colors.Orange },
+      }
+    },
+    NewOrder: { screen: NewOrder,
+      navigationOptions:{
+        tabBarLabel:'NewOrder',
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'ios-images'}/>
+          </View>),
+        activeColor: Colors.Blue,
+        inactiveColor: Colors.InactiveBlue,
+        barStyle: { backgroundColor: Colors.LightBlue },
+      }
+    }
+  },
+  {
+    initialRouteName: "Dashboard",
+    activeColor: Colors.White,
+    inactiveColor: Colors.Green,
+    barStyle: { backgroundColor: Colors.InactiveGreen },
+  },
+);
 
-export default createAppContainer(TabNavigator);  
+export default createAppContainer(TabNavigator);
