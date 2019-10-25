9dd936078c724a4a0e5ba46206319024c3cb1475
GangadharSwami <gangadhar.mswami@gmail.com>
2019-10-25 11:25:28 +0530

    added main auth navigator

--- app/index.android.js
+++ app/index.android.js
@@ -1,3 +1,12 @@
-import Login from './views/Login/index'
+import Login from './views/Login/index';
+import Home from './views/Home/index';
 
-export default Login;
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+const Auth_stack = createSwitchNavigator({
+  Home: { screen: Home } ,
+  Login: { screen: Login }
+}, {
+  initialRouteName: 'Login'
+});
+export default createAppContainer(Auth_stack);
