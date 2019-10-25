2804eeb0a8de3d4d53b2422ac36bfb13ed1ab17c
GangadharSwami <gangadhar.mswami@gmail.com>
2019-10-25 16:08:22 +0530

    added client modal to select clients

--- app/views/NewOrder/index.js
+++ app/views/NewOrder/index.js
@@ -4,6 +4,7 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Colors from '../../helpers/colors';
 import styles from './styles';
 import { Input, Button } from 'react-native-elements';
+import ClientsModal from './selectClientModal.component';
 
 export default class NewOrder extends Component{
   static navigationOptions = {
@@ -17,6 +18,11 @@ export default class NewOrder extends Component{
     barStyle: { backgroundColor: Colors.LightBlue },
   }
 
+  constructor(props){
+    super(props)
+    this.state = { modalVisible: false, selectedClient: ''}
+  }
+
   _renderIcon = (name) => {
     return(
       <Icon
@@ -27,22 +33,40 @@ export default class NewOrder extends Component{
     )
   }
 
+  selectClient = (client) => {
+    this.setState({ selectedClient: client, modalVisible: false })
+  }
+
+  toggleModal = () => {
+    this.setState({ modalVisible: !this.state.modalVisible })
+  }
+
   render() {
     return(
       <View style={ styles.mainContainer }> 
+        <ClientsModal 
+          modalVisible = {this.state.modalVisible}
+          selectedClient = { this.state.selectedClient}
+          selectClient = { this.selectClient.bind(this) }
+          toggleModal = {this.toggleModal.bind(this)}
+          _renderIcon = { this._renderIcon.bind(this)}
+        />
         <ScrollView>
           <View style={ styles.clientNameContainer }>
             <Input
               placeholder='Select Client / Enter name'
               leftIcon={ this._renderIcon("account") }
               containerStyle={{width: '80%', padding: 0}}
+              editable={false}
+              value = {this.state.selectedClient}
             />
             <Button
               icon={ this._renderIcon('account-plus') }
               type="outline"
-              title="Add"
+              title="Select"
               iconContainerStyle={{ paddingRight: 20}}
               containerStyle = {{width: '20%', justifyContent: 'center'}}
+              onPress= { this.toggleModal }
             />
           </View>
         </ScrollView>
