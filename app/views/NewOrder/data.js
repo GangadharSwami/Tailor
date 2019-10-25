1c0651830154c6a469ba4cc58402380259c1949c
GangadharSwami <gangadhar.mswami@gmail.com>
2019-10-25 16:07:19 +0530

    added multiple data

--- app/views/NewOrder/data.js
+++ app/views/NewOrder/data.js
@@ -1,18 +1,66 @@
 export const Clients = [
   {
-    id: 1,
+    id: "1",
     name: "Gangadhar",
     mobile: '1234567890'
   },{
-    id: 2,
+    id: "2",
     name: "Sagar",
     mobile: '098765421'
   },{
-    id: 3,
+    id: "3",
     name: "client 3",
     mobile: '57890431'
   },{
-    id: 4,
+    id: "4",
+    name: "client 5",
+    mobile: '03743828'
+  },{
+    id: "5",
+    name: "Gangadhar",
+    mobile: '1234567890'
+  },{
+    id: "12",
+    name: "Sagar",
+    mobile: '098765421'
+  },{
+    id: "13",
+    name: "client 3",
+    mobile: '57890431'
+  },{
+    id: "14",
+    name: "client 5",
+    mobile: '03743828'
+  },{
+    id: "21",
+    name: "Gangadhar",
+    mobile: '1234567890'
+  },{
+    id: "22",
+    name: "Sagar",
+    mobile: '098765421'
+  },{
+    id: "23",
+    name: "client 3",
+    mobile: '57890431'
+  },{
+    id: "24",
+    name: "client 5",
+    mobile: '03743828'
+  },{
+    id: "31",
+    name: "Gangadhar",
+    mobile: '1234567890'
+  },{
+    id: "32",
+    name: "Sagar",
+    mobile: '098765421'
+  },{
+    id: "33",
+    name: "client 3",
+    mobile: '57890431'
+  },{
+    id: "34",
     name: "client 5",
     mobile: '03743828'
   }
